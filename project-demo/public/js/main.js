a610100ea8549b5cad09ccf97d2e0d2c6a870e82
nhine99 <104955711+nhine99@users.noreply.github.com>
2022-08-05 10:49:21 +0700

    Tải lên

---
+++ project-demo/public/js/main.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){
+    $('.banner').slick({
+        autoplay:true,
+        arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
+});
+
+$('.testimonial-slick').slick({
+    centerMode: true,
+    centerPadding: '250px',
+    slidesToShow: 1,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa          fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '250px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '250px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+// cart sidebar
+
+let headerCart = document.querySelector(".header-cart")
+let cartSidebarClose = document.querySelector(".cart-sidebar-close")
+let cartSidebar = document.querySelector(".cart-sidebar")
+console.log(headerCart)
+console.log(cartSidebar)
+console.log(headerCart)
+headerCart.addEventListener("click",() =>{
+  cartSidebar.classList.add("cart-sidebar-active")
+})
+
+cartSidebarClose.addEventListener('click',() =>{
+  cartSidebar.classList.remove("cart-sidebar-active")
+})
